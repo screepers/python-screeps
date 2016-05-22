45443b3a8799cb87411bfe90c37b915b1cf774c8
Robert Hafner <tedivm@tedivm.com>
2016-05-22 11:29:32 -0700

    Added package/module files

---
+++ screeps/__init__.py
@@ -0,0 +1,3 @@
+from screeps import *
+
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Screeps API',
+    'author': 'dzhu, tedivm',
+    'url': 'https://github.com/tedivm/screeps-api',
+    'download_url': 'https://github.com/tedivm/screeps-api/releases',
+    'author_email': 'tedivm@tedivm.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['screeps'],
+    'scripts': [],
+    'name': 'screeps'
+}
+
+setup(**config)
