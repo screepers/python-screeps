c300073133dcd08d2d5f100a1a006bda60d9ce05
Robert Hafner <tedivm@tedivm.com>
2016-05-22 11:48:22 -0700

    added requires requirement to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'https://github.com/tedivm/screeps-api/releases',
     'author_email': 'tedivm@tedivm.com',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': ['nose', 'requires'],
     'packages': ['screeps'],
     'scripts': [],
     'name': 'screeps'
