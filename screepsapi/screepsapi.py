screepers/python-screeps

commit f47f5d6a6f83c363b205bfa0d0d96c73948618ff

Update the experimental/pvp API call to include the 'start' params argument, and add documentation for both invocations of the API call in Endpoints.

screepsapi/screepsapi.py
@@ -164,9 +164,9 @@ class API(object):
 
     #### battle info methods
 
-    def battles(self, interval=None, sinceTick=None):
+    def battles(self, interval=None, start=None):
         if sinceTick is not None:
-            return self.get('experimental/pvp', sinceTick=sinceTick)
+            return self.get('experimental/pvp', start=start)
         if interval is not None:
             return self.get('experimental/pvp', interval=interval)
         return False

commit 84a44c2cace522a59a9a0a0cf2ed6a579779b06a

Fix bug introduced in f47f5d6a6f83c363b205bfa0d0d96c73948618ff
My previous contribution, f47f5d6a6f83c363b205bfa0d0d96c73948618ff, seems to have completely broken the battles() API method!

My apologies for this, I seem to not have successfully tested it until now.

screepsapi/screepsapi.py
@@ -172,7 +172,7 @@ class API(object):
     #### battle info methods
 
     def battles(self, interval=None, start=None):
-        if sinceTick is not None:
+        if start is not None:
             return self.get('experimental/pvp', start=start)
         if interval is not None:
             return self.get('experimental/pvp', interval=interval)